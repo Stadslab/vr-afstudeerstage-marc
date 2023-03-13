c79f0a635921c86d987b2f3f26334f6e56e67ac5
Marc vd Made <0996549@hr.nl>
2023-03-13 18:52:49 +0100

    added whiteboard

---
+++ VR-voor-stadslab/Assets/Scripts/Whiteboard/WhiteboardMarker.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WhiteboardMarker : MonoBehaviour
+{
+    [SerializeField]
+    private Transform tip;
+
+    [SerializeField]
+    private int penSize = 5;
+
+    private Renderer _renderer;
+    private Color[] _colors;
+    private float _tipHeight;
+
+    private RaycastHit _touch;
+    private Whiteboard _whiteboard;
+    private Vector2 _touchPosition, _lastTouchedPosition;
+    private bool _touchedLastFrame;
+    private Quaternion _lastTouchedRotation;
+
+
+    void Start()
+    {
+        _renderer = tip.GetComponent<Renderer>();
+        _colors = Enumerable.Repeat(_renderer.material.color, penSize * penSize).ToArray();
+        _tipHeight = tip.localScale.y;
+    }
+
+    void Update()
+    {
+        Draw();
+    }
+
+    private void Draw()
+    {
+        if (Physics.Raycast(tip.position, transform.up, out _touch, _tipHeight))
+        {
+            if (_touch.transform.CompareTag("Whiteboard"))
+            {
+                if (_whiteboard == null)
+                {
+                    _whiteboard = _touch.transform.GetComponent<Whiteboard>();
+                }
+
+                _touchPosition = new Vector2(_touch.textureCoord.x, _touch.textureCoord.y);
+
+                var x = (int)(_touchPosition.x * _whiteboard.textureSize.x - penSize/2);
+                var y = (int)(_touchPosition.y * _whiteboard.textureSize.y - penSize/2);
+
+                if (y < 0 || y > _whiteboard.textureSize.y || x < 0 || x > _whiteboard.textureSize.x) return;
+
+                if (_touchedLastFrame)
+                {
+                    _whiteboard.texture.SetPixels(x, y, penSize, penSize, _colors);
+
+                    for (float f = 0.01f; f < 1.00f; f += 0.01f)
+                    {
+                        var lerpX = (int)Mathf.Lerp(_lastTouchedPosition.x, x, f);
+                        var lerpY = (int)Mathf.Lerp(_lastTouchedPosition.y, y, f);
+                        _whiteboard.texture.SetPixels(lerpX, lerpY, penSize, penSize, _colors);
+                    }
+
+                    transform.rotation = _lastTouchedRotation;
+
+                    _whiteboard.texture.Apply();
+                }
+
+                _lastTouchedPosition = new Vector2(x, y);
+                _lastTouchedRotation = transform.rotation;
+                _touchedLastFrame = true;
+                return;
+            }
+        }
+
+        _whiteboard = null;
+        _touchedLastFrame = false;
+    }
+}
