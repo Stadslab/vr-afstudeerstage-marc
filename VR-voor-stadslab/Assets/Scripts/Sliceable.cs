b3110596a133c9b4109487f386131e64f8250ebf
Marc vd Made <0996549@hr.nl>
2023-03-21 19:02:44 +0100

    added slicer demo

---
+++ VR-voor-stadslab/Assets/Scripts/Sliceable.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using UnityEngine;
+
+public class Sliceable : MonoBehaviour
+{
+    [SerializeField]
+    private bool _isSolid = true;
+
+    [SerializeField]
+    private bool _reverseWindTriangles = false;
+
+    [SerializeField]
+    private bool _useGravity = false;
+
+    [SerializeField]
+    private bool _isKinematic = false;
+
+    [SerializeField]
+    private bool _shareVertices = false;
+
+    [SerializeField]
+    private bool _smoothVertices = false;
+    [SerializeField]
+    private bool _canGrab = false;
+
+    public bool IsSolid
+    {
+        get
+        {
+            return _isSolid;
+        }
+        set
+        {
+            _isSolid = value;
+        }
+    }
+
+    public bool ReverseWireTriangles
+    {
+        get
+        {
+            return _reverseWindTriangles;
+        }
+        set
+        {
+            _reverseWindTriangles = value;
+        }
+    }
+
+    public bool UseGravity 
+    {
+        get
+        {
+            return _useGravity;
+        }
+        set
+        {
+            _useGravity = value;
+        }
+    }
+
+    public bool IsKinematic 
+    {
+        get
+        {
+            return _isKinematic;
+        }
+        set
+        {
+            _isKinematic = value;
+        }
+    }
+
+    public bool ShareVertices 
+    {
+        get
+        {
+            return _shareVertices;
+        }
+        set
+        {
+            _shareVertices = value;
+        }
+    }
+
+    public bool SmoothVertices 
+    {
+        get
+        {
+            return _smoothVertices;
+        }
+        set
+        {
+            _smoothVertices = value;
+        }
+    }
+    public bool CanGrab 
+    {
+        get
+        {
+            return _canGrab;
+        }
+        set
+        {
+            _canGrab = value;
+        }
+    }
+
+}
---
+++ VR-voor-stadslab/Assets/Scripts/Slicer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+namespace Assets.Scripts
+{
+    class Slicer
+    {
+        /// <summary>
+        /// Slice the object by the plane 
+        /// </summary>
+        /// <param name="plane"></param>
+        /// <param name="objectToCut"></param>
+        /// <returns></returns>
+        public static GameObject[] Slice(Plane plane, GameObject objectToCut)
+        {            
+            //Get the current mesh and its verts and tris
+            Mesh mesh = objectToCut.GetComponent<MeshFilter>().mesh;
+            var a = mesh.GetSubMesh(0);
+            Sliceable sliceable = objectToCut.GetComponent<Sliceable>();
+
+            if(sliceable == null)
+            {
+                throw new NotSupportedException("Cannot slice non sliceable object, add the sliceable script to the object or inherit from sliceable to support slicing");
+            }
+            
+            //Create left and right slice of hollow object
+            SlicesMetadata slicesMeta = new SlicesMetadata(plane, mesh, sliceable.IsSolid, sliceable.ReverseWireTriangles, sliceable.ShareVertices, sliceable.SmoothVertices);            
+
+            GameObject positiveObject = CreateMeshGameObject(objectToCut);
+            positiveObject.name = string.Format("{0}_positive", objectToCut.name);
+
+            GameObject negativeObject = CreateMeshGameObject(objectToCut);
+            negativeObject.name = string.Format("{0}_negative", objectToCut.name);
+
+            var positiveSideMeshData = slicesMeta.PositiveSideMesh;
+            var negativeSideMeshData = slicesMeta.NegativeSideMesh;
+
+            positiveObject.GetComponent<MeshFilter>().mesh = positiveSideMeshData;
+            negativeObject.GetComponent<MeshFilter>().mesh = negativeSideMeshData;
+
+            SetupCollidersAndRigidBodys(ref positiveObject, positiveSideMeshData, sliceable.UseGravity, sliceable.IsKinematic, sliceable.CanGrab);
+            SetupCollidersAndRigidBodys(ref negativeObject, negativeSideMeshData, sliceable.UseGravity, sliceable.IsKinematic, sliceable.CanGrab);
+
+            return new GameObject[] { positiveObject, negativeObject};
+        }        
+
+        /// <summary>
+        /// Creates the default mesh game object.
+        /// </summary>
+        /// <param name="originalObject">The original object.</param>
+        /// <returns></returns>
+        private static GameObject CreateMeshGameObject(GameObject originalObject)
+        {
+            var originalMaterial = originalObject.GetComponent<MeshRenderer>().materials;
+
+            GameObject meshGameObject = new GameObject();
+            Sliceable originalSliceable = originalObject.GetComponent<Sliceable>();
+
+            meshGameObject.AddComponent<MeshFilter>();
+            meshGameObject.AddComponent<MeshRenderer>();
+            Sliceable sliceable = meshGameObject.AddComponent<Sliceable>();
+
+
+            sliceable.IsSolid = originalSliceable.IsSolid;
+            sliceable.ReverseWireTriangles = originalSliceable.ReverseWireTriangles;
+            sliceable.UseGravity = originalSliceable.UseGravity;
+            sliceable.IsKinematic = originalSliceable.IsKinematic;
+            sliceable.CanGrab = originalSliceable.CanGrab;
+
+            meshGameObject.GetComponent<MeshRenderer>().materials = originalMaterial;
+
+            meshGameObject.transform.localScale = originalObject.transform.localScale;
+            meshGameObject.transform.rotation = originalObject.transform.rotation;
+            meshGameObject.transform.position = originalObject.transform.position;
+
+            meshGameObject.tag = originalObject.tag;
+
+            return meshGameObject;
+        }
+
+        /// <summary>
+        /// Add mesh collider and rigid body to game object
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <param name="mesh"></param>
+        private static void SetupCollidersAndRigidBodys(ref GameObject gameObject, Mesh mesh, bool useGravity, bool isKinematic, bool canGrab)
+        {                     
+            MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
+            meshCollider.sharedMesh = mesh;
+            meshCollider.convex = true;
+
+
+            var rb = gameObject.AddComponent<Rigidbody>();
+            if (canGrab)
+                gameObject.AddComponent<XRGrabInteractable>();
+            rb.useGravity = useGravity;
+            rb.isKinematic = isKinematic;
+        }
+    }
+}
