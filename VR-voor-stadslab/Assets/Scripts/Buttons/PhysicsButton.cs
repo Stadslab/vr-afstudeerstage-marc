096f3146b9d0bcd9d8a1a46f250ade4b03376002
Marc vd Made <0996549@hr.nl>
2023-03-30 11:30:48 +0200

    created pushable button

---
+++ VR-voor-stadslab/Assets/Scripts/Buttons/PhysicsButton.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PhysicsButton : MonoBehaviour
+{
+    [Header("Button Settings")]
+    [SerializeField] private float threshold = .1f;
+    [SerializeField] private float deadZone = 0.025f;
+
+    private bool _isPressed;
+    private Vector3 _startPos;
+    private ConfigurableJoint _joint;
+
+    [Header("Events")]
+    public UnityEvent onPressed, onReleased;
+
+    void Start()
+    {
+        _startPos = transform.localPosition;
+        _joint = GetComponent<ConfigurableJoint>();
+    }
+
+    void Update()
+    {
+        if (!_isPressed && GetValue() + threshold >= 1)
+            Pressed();
+        if (_isPressed && GetValue() - threshold <= 0)
+            Released();
+    }
+
+    private float GetValue()
+    {
+        var value = Vector3.Distance(_startPos, transform.localPosition) / _joint.linearLimit.limit;
+        if (Mathf.Abs(value) < deadZone)
+            value = 0;
+
+        return Mathf.Clamp(value, -1f, 1f);
+    }
+
+    private void Pressed()
+    {
+        _isPressed = true;
+        onPressed.Invoke();
+        Debug.Log("Pressed");
+    }
+
+    private void Released()
+    {
+        _isPressed = false;
+        onReleased.Invoke();
+        Debug.Log("Released");
+    }
+}
